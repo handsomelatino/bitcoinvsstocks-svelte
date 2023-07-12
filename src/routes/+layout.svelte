e48913b5e7571af07b076db8ec7e5042b4bf9787
Trevor I <handsome.latino@protonmail.com>
2023-07-12 11:59:10 +0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<nav>
+  <a href='/'>Home</a>
+  <a href='/about'>About</a>
+</nav>
+
+<slot />
+
+<style>
+  :global(body) {
+    margin: 0;
+    background-color: black;
+    color: white;
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+   <title>ABOUT</title>
+</svelte:head>
+
+<h2>ABOUT PAGE</h2>

0aefe516a157fdcdd815b9f276410b0407a38069
Trevor I <handsome.latino@protonmail.com>
2024-02-29 16:27:05 -0600

    add popular stocks, remove splash on mobiles, align calculator

--- src/routes/Calculator.svelte
+++ src/routes/Calculator.svelte
@@ -1,5 +1,6 @@
 <script>
   import { PUBLIC_API_URL } from '$env/static/public';
+  import POPULAR_STOCKS from '../utils/popular_stocks';
 
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -37,11 +38,11 @@
 
   let COMMON_STOCKS = [
     { id: "^GSPC", label: "S&P 500" },
-    { id:  "^DJI", label: "Dow Jones" },
+    { id: "^DJI", label: "Dow Jones" },
     { id: "^IXIC", label: "NASDAQ" },
     { id: "GC=F",  label: "Gold" },
     { id: "SI=F",  label: "Silver" },
-  ]
+  ];
 
   const handleCalculate = async () => {
     fetching = true;
@@ -53,46 +54,46 @@
 </script>
 
 <section id="calculate">
+  <h2><span class='btc'>Bitcoin</span> <small>vs</small> <span class='stocks'>Stocks</span></h2>
 
   <div class="inputs">
-    <p>
-      <span>Saving </span>
-        <span class='dollar-container'>
-          <span class='dollar-sign'>$</span>
-          <input bind:value={amount} type="number" placeholder="Dollar" />
-        </span>
+    <div class="grid">
+      <span>Saving / Investing </span>
+      <span class='dollar-container'>
+        <span class='dollar-sign'>$</span>
+        <input bind:value={amount} type="number" placeholder="Dollar" />
+      </span>
+
       <span>every </span>
+
       <select bind:value={frequency}>
         {#each FREQUENCIES as f}
           <option value={f.id}>{f.text}</option>
         {/each}
       </select>
-    </p>
-
-    <!-- <p>
-    </p> -->
-
-    <p>
+      
       <span>For the past </span>
       <select bind:value={years}>
         {#each YEARS as y}
-          <option value={y.id}>{y.text}</option>
+        <option value={y.id}>{y.text}</option>
         {/each}
       </select>
-      <span>in <strong>Bitcoin</strong></span>
-    </p>
-
-    <p><span>compared to </span>
-      <select bind:value={ticker}>
-        {#each COMMON_STOCKS as s}
-          <option value={s.id}>{s.label}</option>
-        {/each}
-      </select>
-      <!-- <input bind:value={ticker} placeholder="Stock ticker" /></p> -->
-  </div>
-
-  <button class='calculate' disabled={fetching} on:click={handleCalculate}>Calculate</button>
-
+    </div>
+      
+      <p><span>compared to </span>
+        <select class="tickers" bind:value={ticker}>
+          {#each COMMON_STOCKS as s}
+            <option value={s.id}>{s.label}</option>
+          {/each}
+          {#each POPULAR_STOCKS as s}
+            <option value={s.id}>{s.label}</option>
+          {/each}
+        </select>
+        <!-- <input bind:value={ticker} placeholder="Stock ticker" /></p> -->
+      </div>
+      
+      <button class='calculate' disabled={fetching} on:click={handleCalculate}>Calculate</button>
+      
   {#if dcaData}
     <p>Saving <strong>{formatter.format(amount)} { frequency === 'b' ? 'every' : 'per' } {FREQUENCIES.find(f => f.id === frequency).text}</strong> for the past {years} years until today results in:</p>
 
@@ -100,7 +101,7 @@
       <tr>
         <th />
         <th>BTC</th>
-        <th>{ COMMON_STOCKS.find(s => s.id === dcaData.Stocks.ticker).label }</th>
+        <th>{ COMMON_STOCKS.find(s => s.id === dcaData.Stocks.ticker)?.label || dcaData.Stocks.ticker }</th>
       </tr>
 
       <tr>
@@ -122,8 +123,8 @@
       </tr>
 
       <tr>
-        <td>BTC Amount</td>
-        <td class="centered">{ dcaData.Bitcoin.btc_amount }</td>
+        <td>Stacked</td>
+        <td class="centered">{ dcaData.Bitcoin.btc_amount } BTC</td>
         <td class="centered"> — </td>
       </tr>
     </table>
@@ -140,6 +141,45 @@
     margin: 0 auto;
     min-height: 100vh;
     text-align: center;
+
+    @media screen and (max-width: 480px) {
+      font-size: 16px;
+    }
+  }
+
+  h2 {
+    font-family: var(--alfa-slab);
+    font-weight: normal;
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 20px;
+    margin: 12px auto 32px;
+    letter-spacing: 1px;
+  }
+
+  h2 .btc {
+    color: var(--emphasis);
+    text-shadow: 0 2px 0 var(--emphasis-2);
+  }
+
+  h2 .stocks {
+    text-shadow: 0 2px 0 var(--logo-shadow);
+  }
+
+  small {
+    font-size: 12px;
+    vertical-align: middle;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 12px 16px;
+    align-items: center;
+  }
+
+  .grid span {
+    text-align: left;
   }
 
   .inputs p strong {
@@ -162,10 +202,6 @@
     padding-inline-start: 32px;
   }
 
-  input {
-    width: 140px;
-  }
-
   input, select {
     border: none;
     font-family: var(--maven-pro);
@@ -174,15 +210,22 @@
     background-color: var(--light-bg);
     color: var(--text-color);
     font-size: 24px;
-    /* width: 140px; */
-    margin: 0 12px;
-    /* font-weight: bold; */
     text-align: right;
+    width: 100%;
+
+    @media screen and (max-width: 480px) {
+      font-size: 16px;
+    }
+  }
+
+  select.tickers {
+    margin-top: 8px;
   }
 
   button.calculate {
     all: unset;
     cursor: pointer;
+    box-sizing: border-box;
     text-decoration: none;
     color: var(--main-bg);
     background-color: var(--emphasis);
@@ -192,7 +235,13 @@
     text-transform: uppercase;
     font-weight: bold;
     transition: filter 0.3s;
-    margin: 20px 0;
+    margin: 12px 0;
+
+    @media screen and (max-width: 480px) {
+      display: block;
+      width: 100%;
+      font-size: 20px;
+    }
   }
 
   button.calculate:disabled {
@@ -209,14 +258,27 @@
     margin: 0 auto;
     font-size: 24px;
     border-collapse: collapse;
+
+    @media screen and (max-width: 480px) {
+      font-size: 16px;
+      width: 100%;
+    }
   }
 
   th {
     font-size: 28px;
+
+    @media screen and (max-width: 480px) {
+      font-size: 18px;
+    }
   }
 
   th, td {
     padding: 16px 28px;
+
+    @media screen and (max-width: 480px) {
+      padding: 8px 16px;
+    }
   }
 
   td {
